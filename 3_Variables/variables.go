42b6ca803e4062397a50e59aaf5efade0196c87e
avaneesh <avaneesh.gupta5@gmail.com>
2025-10-25 22:44:10 +0530

    variables and constants in go

---
+++ 3_Variables/variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//how to declare variables
+	// var name string = "goLang"
+
+	// type infer krta hai go if wo do not provide any type
+	var name = "goLang"
+
+	var isAdult = true
+
+	var age = 30
+
+	// shorthand syntax
+
+	naam := "goLang"
+
+	fmt.Println(name, isAdult, age, naam)
+}
---
+++ 4_constants/constants.go
@@ -0,0 +1,8 @@
+package main
+
+func main()  {
+	//variables whose value can not changes later 
+
+	const name = "goLang"
+
+}

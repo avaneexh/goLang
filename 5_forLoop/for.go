920cb3376e44a681f13039000b8f3afd348785fe
avaneesh <avaneesh.gupta5@gmail.com>
2025-10-26 22:47:12 +0530

    for loops

---
+++ 5_forLoop/for.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// only for loop is in golang no wilile loop is in go lang
+	// for -> only construct in go for looping
+
+	// we implement while loop using for
+
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// infinite loop
+
+	// for {
+	// 	fmt.Println("1")
+	// }
+
+	// classic for loop 
+
+	for i:=0; i<=3; i++ {
+		
+		fmt.Println(i)
+	}
+
+}

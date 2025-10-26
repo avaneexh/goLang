b2e2a9f1e7f12c9d62f0720734490e33b7258a18
avaneesh <avaneesh.gupta5@gmail.com>
2025-10-26 21:13:50 +0530

    constants

--- 4_constants/constants.go
+++ 4_constants/constants.go
@@ -1,8 +1,16 @@
 package main
 
-func main()  {
-	//variables whose value can not changes later 
+import "fmt"
+
+func main() {
+	//variables whose value can not changes later
 
 	const name = "goLang"
+	const port = 5000
+	const host = "localhost"
+
+	fmt.Println(name, port, host )
+
+	// host = "server"  // not allowed in go
 
 }

cad896c831da8b7bbd2211dfef051b7b0d10316c
avaneesh <avaneesh.gupta5@gmail.com>
2026-06-08 08:59:57 +0530

    Add initial functions.go file with commented-out function examples

---
+++ 12_Functions/functions.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// func add(a int, b int) int {
+// 	return a + b
+// }
+
+// func getLanguages() (string, string, string, bool) {
+// 	return "golang", "js", "c", true
+// }
+
+
+func main() {
+	// result := add(3, 5)
+	// fmt.Println("Result",result)
+	// lang1, lang2, _, tf := getLanguages()
+	// fmt.Println(getLanguages())
+	// fmt.Println(lang1)
+	// fmt.Println(lang2)
+	// fmt.Println(tf)
+	
+}
